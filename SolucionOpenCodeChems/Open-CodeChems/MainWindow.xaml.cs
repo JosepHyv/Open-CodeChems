7ec8ecd043cf1766c82b321aba63b6a1137bd8b4
Josephyv <josephynimoto@gmail.com>
2022-09-23 11:58:07 -0500

    redirect project

--- SolucionOpenCodeChems/Open-CodeChems/MainWindow.xaml.cs
+++ SolucionOpenCodeChems/Open-CodeChems/MainWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace Open_CodeChems
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
