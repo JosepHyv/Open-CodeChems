013b5283bb27deae25687b84837d533fe8151a4a
JosepHyv <josephynimoto@gmail.com>
2022-10-27 10:43:21 -0500

    created Interfaces and Contexts

---
+++ OpenCodeChems.Server/OpenCodeChems.BusinessLogic/UserManagement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using OpenCodeChems.BusinessLogic.Interface;
+using OpenCodeChems.DataAccess;
+
+namespace OpenCodeChems.BusinessLogic
+{
+    internal class UserManagement : IUserManagement
+    {
+        public bool RegisterUser(User user, string nikname)
+        {
+            bool status = false;
+            using (OpenCodeChemsContext context = new OpenCodeChemsContext())
+            {
+               EntityEntry<User> newUser = context.User.Add(new User
+               {
+                   username = user.username, email = user.email, name = user.name, password = user.password
+               });
+                context.SaveChanges();
+                EntityEntry<Profile> newProfile = context.Profile.Add(new Profile { nickname = user.name, username = user.username });
+                if (newUser != null && newProfile != null)
+                {
+                    status = true;
+                }
+            }
+            return status;
+        }
+
+        public bool Login(string username, string password)
+        {
+            bool status = false;
+            using (OpenCodeChemsContext context = new OpenCodeChemsContext())
+            {
+                int foundUser = (from User in context.User
+                    where User.username == username && User.password == password
+                    select User).Count();
+                if (foundUser > 0)
+                {
+                    status = true;
+                }
+            }
+            return status;
+        }
+    }
+}
--- OpenCodeChems.Server/OpenCodeChems.DataAccess/OpenCodeChemsContext.cs
+++ OpenCodeChems.Server/OpenCodeChems.DataAccess/OpenCodeChemsContext.cs
@@ -21,7 +21,7 @@ namespace OpenCodeChems.DataAccess
     }
     public class User
     {
-        public string userame { get; set; }
+        public string username { get; set; }
         public string password { get; set; }
         public string name { get; set; }
         public string email { get; set; }
