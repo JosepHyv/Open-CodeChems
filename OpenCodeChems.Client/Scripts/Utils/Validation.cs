6bb95c017a122e6db62b2b28769bd59060440171
JosepHyv <josephynimoto@gmail.com>
2022-10-27 11:08:10 -0500

    utils sha256

--- OpenCodeChems.Client/Scripts/Utils/Validation.cs
+++ OpenCodeChems.Client/Scripts/Utils/Validation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,12 @@ namespace OpenCodeChems.Client.Resources
 {
     internal class Validation
     {
+        public string ComputeSHA256Hash(string text)
+        {
+            using (var sha256 = new SHA256Managed())
+            {
+                return BitConverter.ToString(sha256.ComputeHash(Encoding.UTF8.GetBytes(text))).Replace("-", "");
+            }
+        }
     }
 }
