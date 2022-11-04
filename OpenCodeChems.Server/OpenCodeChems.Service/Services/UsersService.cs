03ce333c847b108f64e2ee0f4d6ad1389f4cee76
JosepHyv <josephynimoto@gmail.com>
2022-11-03 23:27:01 -0600

    Services complete

--- OpenCodeChems.Server/OpenCodeChems.Service/Services/UsersService.cs
+++ OpenCodeChems.Server/OpenCodeChems.Service/Services/UsersService.cs
@@ -1,11 +1,27 @@
 ﻿using System.Threading.Tasks;
 using Grpc.Core;
-using OpenCodeChems.Service.Users;
+using static OpenCodeChems.Service.Users;
 
 namespace OpenCodeChems.Service.Services
 {
     public class UsersService : Users.UsersBase
     {
-        
+        private readonly UsersRepository _usersRepository;
+
+        public UsersService(UsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        public override Task<StatusPetitionResponse> RegisterNewUsers(RegisterRequest request, ServerCallContext context)
+        {
+            var newUser = context.GetHttpContext().Items;
+            return base.RegisterNewUsers(request, context);
+        }
+
+        public override Task<StatusPetitionResponse> LoginPlayer(LoginRequest request, ServerCallContext context)
+        {
+            return base.LoginPlayer(request, context);
+        }
     }
 }
