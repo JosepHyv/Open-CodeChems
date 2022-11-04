8f483cc18761ca8e514c38a2e2a0eb7d4252994b
Zinedinne <miguelzinedinne@gmail.com>
2022-11-04 04:16:37 -0600

    solucionado problema de selección

--- OpenCodeChems.Client/Scripts/UserInterface/SpyPlayer.cs
+++ OpenCodeChems.Client/Scripts/UserInterface/SpyPlayer.cs
@@ -10,12 +10,47 @@ public class SpyPlayer : Control
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        GD.Print("Hola mundo en godot");
     }
 
-    public void _on_CodeNamesItemList_item_selected()
+    /*private void _on_CodeNamesItemList_item_selected(InputEventMouseButton e)
     {
+        
         GD.Print("aqui va la palabra");
+        
+        
+        var  itemNode = GetParent().GetNode<ItemList>("SpyPlayer/BackGroundNinePatchRect/CodeNamesItemList");
+        string[] listElements = new string[itemNode.GetItemCount()];
+
+        int elemento = 7;
+        string palabra = "Astroaut";
+
+        itemNode.SetItemText(elemento, palabra);
+
+
+        for(int c = 0 ; c<itemNode.GetItemCount(); c++)
+        {
+            
+                var card = itemNode.GetItemMetadata(c);
+                GD.Print(card);
+        }
+
+        GD.Print("=================="); 
+
+            
+    }*/
+    private void _on_CodeNamesItemList_item_selected(int index)
+    {
+        
+        
+        
+        var  itemNode = GetParent().GetNode<ItemList>("SpyPlayer/BackGroundNinePatchRect/CodeNamesItemList");
+        var  selectedCard = GetParent().GetNode<RichTextLabel>("SpyPlayer/BackGroundNinePatchRect/SelectedCardRichTextLabel");
+        selectedCard.Clear();
+        selectedCard.AddText(itemNode.GetItemText(index));
+        
+
+            
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
