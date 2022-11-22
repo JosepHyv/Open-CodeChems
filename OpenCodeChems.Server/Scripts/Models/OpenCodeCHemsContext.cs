7fd04b8011c5c1bd252eba65748e726ff582015d
JosepHyv <josephynimoto@gmail.com>
2022-11-22 10:10:47 -0600

    solving problem with callbacks

--- OpenCodeChems.Server/Scripts/Models/OpenCodeCHemsContext.cs
+++ OpenCodeChems.Server/Scripts/Models/OpenCodeCHemsContext.cs
@@ -10,43 +10,43 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OpenCodeChems.DataAccess
 {
-    public class OpenCodeChemsContext: DbContext
-    {
-        
+	public class OpenCodeChemsContext: DbContext
+	{
+		
 
-        public DbSet<User> User { get; set; }
+		public DbSet<User> User { get; set; }
 
-        
+		
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server=tcp:openchemsserver.database.windows.net,1433;Initial Catalog=opencodechems;Persist Security Info=False;User ID=chemsito;Password=MasterKey$123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30");   
-        }
-        
-    }
-    public class User
-    {
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=tcp:openchemsserver.database.windows.net,1433;Initial Catalog=opencodechems;Persist Security Info=False;User ID=chemsito;Password=MasterKey$123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30");   
+		}
+		
+	}
+	public class User
+	{
 
-        public User(string username, string password, string name, string email, int victories, int defeats, byte[] imageProfile)
-        {
-            this.username = username;
-            this.password = password;
-            this.name = name;
-            this.email = email;
-            this.victories = victories;
-            this.defeats = defeats;
-            this.imageProfile = imageProfile;
-        }
+		public User(string username, string password, string name, string email, int victories, int defeats, byte[] imageProfile)
+		{
+			this.username = username;
+			this.password = password;
+			this.name = name;
+			this.email = email;
+			this.victories = victories;
+			this.defeats = defeats;
+			this.imageProfile = imageProfile;
+		}
 
-        [Key]
-        public string username { get; set; }
-        public string password { get; set; }
-        public string name { get; set; }
-        public string email { get; set; }
-        public int victories { get; set; }
-        public int defeats { get; set; }
-        public byte[] imageProfile { get; set; }
+		[Key]
+		public string username { get; set; }
+		public string password { get; set; }
+		public string name { get; set; }
+		public string email { get; set; }
+		public int victories { get; set; }
+		public int defeats { get; set; }
+		public byte[] imageProfile { get; set; }
 
-    }
+	}
 
 }
--- OpenCodeChems.Server/Scripts/Network.cs
+++ OpenCodeChems.Server/Scripts/Network.cs
@@ -6,83 +6,83 @@ using System.Data.SqlClient;
 
 public class Network : Node
 {
-    private int DEFAULT_PORT = 5500;
-    private string ADDRESS = "localhost";
-    private int MAX_PLAYERS = 200;
-    private int PEERID = 1;
-    public override void _Ready()
-    {
-        GD.Print("Entrando al server Godot");
-        var server = new NetworkedMultiplayerENet();
-        var result = server.CreateServer(DEFAULT_PORT, MAX_PLAYERS);
-        GD.Print(result);
-        if(result == 0 )
-        {
-            GetTree().NetworkPeer = server;
-            GD.Print($"Hosteando server en {ADDRESS}:{DEFAULT_PORT}.");
-            GD.Print(GetTree().NetworkPeer);
-        }
-        GD.Print($"Estoy escuchando? {GetTree().IsNetworkServer()}");
-        GD.Print($"Mi network ID = {GetTree().GetNetworkUniqueId()}");
-        GetTree().Connect("network_peer_connected", this, nameof(PlayerConnected));
-        GetTree().Connect("network_peer_disconnected", this, nameof(PlayerDisconnected));
+	private int DEFAULT_PORT = 5500;
+	private string ADDRESS = "localhost";
+	private int MAX_PLAYERS = 200;
+	private int PEERID = 1;
+	public override void _Ready()
+	{
+		GD.Print("Entrando al server Godot");
+		var server = new NetworkedMultiplayerENet();
+		var result = server.CreateServer(DEFAULT_PORT, MAX_PLAYERS);
+		GD.Print(result);
+		if(result == 0 )
+		{
+			GetTree().NetworkPeer = server;
+			GD.Print($"Hosteando server en {ADDRESS}:{DEFAULT_PORT}.");
+			GD.Print(GetTree().NetworkPeer);
+		}
+		GD.Print($"Estoy escuchando? {GetTree().IsNetworkServer()}");
+		GD.Print($"Mi network ID = {GetTree().GetNetworkUniqueId()}");
+		GetTree().Connect("network_peer_connected", this, nameof(PlayerConnected));
+		GetTree().Connect("network_peer_disconnected", this, nameof(PlayerDisconnected));
 
-    }
+	}
 
-    private void PlayerConnected(int peerId)
-    {
-        GD.Print($"Jugador = {peerId} Conectado");
-    }
+	private void PlayerConnected(int peerId)
+	{
+		GD.Print($"Jugador = {peerId} Conectado");
+	}
 
-    private void PlayerDisconnected(int peerId)
-    {
-    	GD.Print($"Jugador = {peerId} Desconectado");
-    }
+	private void PlayerDisconnected(int peerId)
+	{
+		GD.Print($"Jugador = {peerId} Desconectado");
+	}
 
-    
-    
-    [Master]
-    private void LoginRequest(string username, string password)
-    {
-        int senderId = GetTree().GetRpcSenderId();
-        UserManagement userManagement = new UserManagement();
-        bool status = false;
-        if(userManagement.Login(username, password) == true)
-        {
-            status = true;
-            RpcId(senderId, "LoginResponse", status);
-            GD.Print($"Player no. {senderId} logged in successfully.");
-        }
-        else
-        {
-            RpcId(senderId, "LoginResponse", status);
-            GD.Print($"Player no. {senderId} logged in failed.");
-        }
-    }
-    [Master]
-    private void RegisterUserRequest(string name, string email, string username, string hashPassword,byte[] imageProfile, int victories, int defeats)
-    {
-        int senderId = GetTree().GetRpcSenderId();
-        UserManagement userManagement = new UserManagement();
-        bool status = false;
-        try
-        {
-            User newUser = new User(username, hashPassword, name, email, victories, defeats, imageProfile);
-            if(userManagement.RegisterUser(newUser) == true)
-            {
-                status = true;
-                RpcId(senderId, "RegisterUserResponse", status);
-                GD.Print($"Player no. {senderId} registered in successfully.");             
-            }
-            else
-            {
-                RpcId(senderId, "RegisterUserResponse", status);
-                GD.Print($"Player no. {senderId} registered in failed.");
-            }
-        }
-        catch 
-        {
-            
-        }
-    }
+	
+	
+	[Master]
+	private void LoginRequest(string username, string password)
+	{
+		int senderId = GetTree().GetRpcSenderId();
+		UserManagement userManagement = new UserManagement();
+		bool status = false;
+		if(userManagement.Login(username, password) == true)
+		{
+			status = true;
+			RpcId(senderId, "LoginSuccesful", status);
+			GD.Print($"Player no. {senderId} logged in successfully.");
+		}
+		else
+		{
+			RpcId(senderId, "LoginFailed", status);
+			GD.Print($"Player no. {senderId} logged in failed.");
+		}
+	}
+	[Master]
+	private void RegisterUserRequest(string name, string email, string username, string hashPassword,byte[] imageProfile, int victories, int defeats)
+	{
+		int senderId = GetTree().GetRpcSenderId();
+		UserManagement userManagement = new UserManagement();
+		bool status = false;
+		try
+		{
+			User newUser = new User(username, hashPassword, name, email, victories, defeats, imageProfile);
+			if(userManagement.RegisterUser(newUser) == true)
+			{
+				status = true;
+				RpcId(senderId, "RegisterUserResponse", status);
+				GD.Print($"Player no. {senderId} registered in successfully.");             
+			}
+			else
+			{
+				RpcId(senderId, "RegisterUserResponse", status);
+				GD.Print($"Player no. {senderId} registered in failed.");
+			}
+		}
+		catch 
+		{
+			
+		}
+	}
 }
