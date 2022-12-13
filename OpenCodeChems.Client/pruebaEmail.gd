a90550393ff6113e6c81faa80a6e0f9fcce3c4f1
Zinedinne <miguelzinedinne@gmail.com>
2022-12-12 23:19:04 -0600

    sending email succesfull

---
+++ OpenCodeChems.Client/pruebaEmail.gd
@@ -0,0 +1,47 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+
+func SendEmail(emailto, emailfrom, subject, body):
+
+	var command_body = [	"$EmailFrom = '%s'" %[emailfrom],
+	"$EmailTo = '%s'" %[emailto],
+	"$Subject = '%s'"%[subject],
+	"$Body = '%s'" %[body],
+	"$SMTPServer = 'smtp.gmail.com'",
+	"$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)",
+	"$SMTPClient.EnableSsl = $true",
+	"$SMTPClient.Credentials = New-Object System.Net.NetworkCredential('codechems@gmail.com', 'cmoyfycjuwexfawp')",
+	"$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)",
+]
+
+	var commands = ""
+	var count = 1
+	for command in len(command_body):
+		if count != len(command_body):
+			commands += command_body[command] + "; "
+		else:
+			commands += command_body[command]
+	
+		count += 1
+
+	var output = []
+	OS.execute("C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe", [commands], true, output)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_TextureButton_pressed():
+	SendEmail("miguelzinedinne@gmail.com","codechems@gmail.com","prueba de email en gdscript","esta es una prueba para enviar un email en gdscript")
