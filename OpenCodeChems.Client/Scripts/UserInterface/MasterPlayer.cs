4c61f45b9eb621114175e4d0d4c78d71f27692fa
josephyv <josephynimoto@gmail.com>
2022-12-18 15:39:03 -0600

    word selection validation

--- OpenCodeChems.Client/Scripts/UserInterface/MasterPlayer.cs
+++ OpenCodeChems.Client/Scripts/UserInterface/MasterPlayer.cs
@@ -47,10 +47,10 @@ public class MasterPlayer : Control
 		for(int c = 0 ; c<itemNode.GetItemCount(); c++)
 		{	
 			var masterDialog = GetParent().GetNode<AcceptDialog>("MasterPlayer/MasterPlayerAcceptDialog");
-			if(pistaPalabra.Text == itemNode.GetItemText(c))
+			if(pistaPalabra.Text.ToLower() == itemNode.GetItemText(c) || pistaPalabra.Text.Length > 10 || pistaPalabra.Text.Any(Char.IsWhiteSpace))
 			{
 				masterDialog.WindowTitle="WARNING";
-				masterDialog.DialogText="REPEATED_WORD";
+				masterDialog.DialogText="INVALID_WORD";
 				masterDialog.Visible = true;
 				wellDone = false;
 			}				
@@ -64,46 +64,5 @@ public class MasterPlayer : Control
 		
 		
 	}
-	/// <summary>
-    /// Loads the card codenames from the right file depending on the internacionalization 
-    /// into an array of strings.
-    /// </summary>
-    /// <returns>An array  with codenames as strings.</returns>
-	private List<string> LoadInternacionalizedCards()
-	{	
-		string path = "";
-		bool exist = true;
-        Godot.File cardValues = new Godot.File();
-		List<string> listGameElements = new List<string>();
-		List<string> listAllElements = new List<string>();
-		if(TranslationServer.GetLocale() == "en")
-		{
-			path = "res://Scenes/Resources/words.txt";
-		}
-		else
-		{
-			path = "res://Scenes/Resources/palabras.txt";
-		}
-		try
-		{
-			cardValues.Open(path, Godot.File.ModeFlags.Read);
-		 	while (!cardValues.EofReached())
-      		{
-         		listAllElements.Add(cardValues.GetLine().Trim());
-     	 	}
-		}
-		catch(FileNotFoundException)
-		{
-      		masterDialog.WindowTitle="ERROR";
-			masterDialog.DialogText="FILE_NOT_FOUND";
-			masterDialog.Visible = true;
-    	}
-
-		Random rand = new Random();
-        listGameElements = listAllElements.OrderBy(_ => rand.Next()).ToList();
-		
-		
-		return listGameElements;
-	}
-
+	
 }
